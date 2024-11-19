da825fc9afacad8dc5199359f0c217bf0ac39793
luis silva <luis.fernando.coelho14@gmail.com>
2024-11-19 01:46:01 -0300

    arduino changes

--- MPJCode/MPJCode.ino
+++ MPJCode/MPJCode.ino
@@ -6,7 +6,8 @@
 #include <ESP32Servo.h>
 #include <WiFiManager.h>
 #include <ArduinoMqttClient.h>
-
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 // Define Struct
 struct Bay {
   int idBay;
@@ -27,17 +28,17 @@ struct Bay {
 };
 
 // Define PINS
-const int pinoReleBomba1 = 16;  // Exemplo: pino GPIO 5
-const int pinoReleBomba2 = 17;  // Exemplo: pino GPIO 18
-const int pinDome1 = 4;         // Exemplo: pino GPIO 13
+const int pinoReleBomba1 = 32;  // Exemplo: pino GPIO 5
+const int pinoReleBomba2 = 33;  // Exemplo: pino GPIO 18
+// const int pinDome1 = 14;        // Exemplo: pino GPIO 13
 //const int pinoServoDireito2 = 14;   // Exemplo: pino GPIO 14
-const int pinDome2 = 0;  // Exemplo: pino GPIO 12
+// const int pinDome2 = 27;  // Exemplo: pino GPIO 12
 //const int pinoServoEsquerdo2 = 27;  // Exemplo: pino GPIO 27
-const int umidadeSensorBaia1 = 34;  // Exemplo: pino GPIO 34
-const int umidadeSensorBaia2 = 35;  // Exemplo: pino GPIO 35
-const int nivelSensor = 32;         // Exemplo: pino GPIO 32
-const int ledBaia1 = 33;            // Exemplo: pino GPIO 19
-const int ledBaia2 = 25;            // Exemplo: pino GPIO 21
+#define umidadeSensorBaia1 A0  // PINO 36
+#define umidadeSensorBaia2 A3  // PINO 39
+// const int nivelSensor = 32;         // Exemplo: pino GPIO 32
+const int ledBaia1 = 17;  // Exemplo: pino GPIO 19
+const int ledBaia2 = 16;   // Exemplo: pino GPIO 21
 
 // Define variables
 Bay baia[2];
@@ -50,15 +51,27 @@ String topic;
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 
+unsigned long messageSelector=0;
+
 // Cria um buffer de memória para o JSON. O tamanho é ajustado ao tamanho do JSON.
 StaticJsonDocument<3072> doc;
 
+//Leitor LCD
+LiquidCrystal_I2C LCD = LiquidCrystal_I2C(0x27, 16, 2);
+unsigned long previousMillis = 0;  // Tempo de referência
+const long interval = 10000;       // Intervalo de 10 segundos
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println("Hello, ESP32!");
   Serial.println("Inicializando o SD card...");
-
+  LCD.init();
+  LCD.backlight();  // Liga a luz de fundo
+  LCD.setCursor(0, 0);
+  LCD.print("Bem vindo ao Meu");
+  LCD.setCursor(0, 1);
+  LCD.print("Pequeno Jardim");
+  // delay(2000);
   // Verifica se o cartão SD foi inicializado corretamente
   if (!SD.begin(chipSelect)) {
     Serial.println("Falha na inicialização do SD card!");
@@ -73,24 +86,43 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for (int i = 0; i < 2; i++) {
-    controlHumidity(&baia[i]);
-    controlLight(&baia[i]);
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    for (int i = 0; i < 2; i++) {
+      controlHumidity(&baia[i]);
+      controlLight(&baia[i]);
+    }
+    // String docSerializedJson = serializeJsonStruct();
+    // Serial.println(docSerializedJson);
+    writeFile("baysConfig");
+    mqttClient.poll();
+    recieveMessage();
+    sendMessage();
+    changeLCDMessage();
   }
-  // String docSerializedJson = serializeJsonStruct();
-  // Serial.println(docSerializedJson);
-  writeFile("baysConfig");
-  mqttClient.poll();
-  recieveMessage();
-  sendMessage();
-  delay(5000);
+  
 }
-
+void changeLCDMessage(){
+  messageSelector++;
+  LCD.clear();
+  if(messageSelector%2==0){
+  LCD.setCursor(0, 0);
+  LCD.print("Meu Pequeno");
+  LCD.setCursor(0, 1);
+  LCD.print("Jardim");
+  }else{
+  LCD.setCursor(0, 0);
+  LCD.print(WiFi.SSID());
+  LCD.setCursor(0, 1);
+  LCD.print(WiFi.localIP());
+  
+  }
+}
 void configBay(String configs) {
   if (configs == "") {
-    baia[0] = { 0, 16, 33, 0, 34, false, false, false, 55, 600, 150, 1000 };
-    baia[1] = { 1, 17, 255, 1, 35, false, false, false, 55.2, 600, 400, 1000 };
+    baia[0] = { 0, pinoReleBomba1, ledBaia1, 0, umidadeSensorBaia1, false, false, false, 0, 4000, 0, 21600 };
+    baia[1] = { 1, pinoReleBomba2, ledBaia2, 1, umidadeSensorBaia2, false, false, false, 0, 4000, 0, 21600 };
     writeFile("baysConfig");
   } else {
     if (convertJson(configs)) {
@@ -126,12 +158,12 @@ void updateBay(String configs) {
     Serial.print("Erro na leitura do JSON: ");
     Serial.println(error.f_str());
   } else {
-     for (JsonObject module : docRecived.as<JsonArray>()) {
-        int idBay = module["idBay"];
-        baia[idBay].umySensorValueDefault = module["umySensorValueDefault"];
-        baia[idBay].lightTimePickerDefault = module["lightTimePickerDefault"];
-      }
-      Serial.println(serializeJsonStruct());
+    for (JsonObject module : docRecived.as<JsonArray>()) {
+      int idBay = module["idBay"];
+      baia[idBay].umySensorValueDefault = module["umySensorValueDefault"];
+      baia[idBay].lightTimePickerDefault = module["lightTimePickerDefault"];
+    }
+    Serial.println(serializeJsonStruct());
   }
 }
 void writeFile(String nameArchive) {
@@ -144,10 +176,15 @@ void writeFile(String nameArchive) {
     String docSerializedJson = serializeJsonStruct();
     dataFile.println(docSerializedJson);
     // Fecha o arquivo após a escrita
+    dataFile.flush();
     dataFile.close();
     Serial.println("Arquivo escrito e fechado com sucesso.");
   } else {
     // Se houver erro ao abrir o arquivo, exibe uma mensagem de erro
+    SD.end();  // Finalize a conexão com o SD
+    if (SD.begin(chipSelect)) {
+      Serial.println("sd reiniciado");
+    }
     Serial.println("Erro ao abrir o arquivo para escrita.");
   }
 }
@@ -237,12 +274,29 @@ String serializeJsonStructToMqtt() {
 void connectWifi() {
   WiFiManager wifiManager;
   wifiManager.setConfigPortalTimeout(240);
+  LCD.clear();
+  LCD.setCursor(0, 0);
+  LCD.print("Conecte ao Wifi");
+  LCD.setCursor(0, 1);
+  LCD.print("MeuPequenoJardim");
   if (!wifiManager.autoConnect("MeuPequenoJardim")) {
     Serial.println(F("Falha na conexao. Resetar e tentar novamente..."));
-    delay(3000);
+    // delay(3000);
     ESP.restart();
-    delay(5000);
+    // delay(5000);
   }
+  // delay(2000);
+  LCD.clear();
+  LCD.setCursor(0, 0);
+  LCD.print("Conectado na");
+  LCD.setCursor(0, 1);
+  LCD.print("rede Wifi.");
+  // delay(3000);
+  LCD.clear();
+  LCD.setCursor(0, 0);
+  LCD.print(WiFi.SSID());
+  LCD.setCursor(0, 1);
+  LCD.print(WiFi.localIP());
   Serial.println(F("Conectado na rede Wifi."));
   Serial.print(F("Endereco IP: "));
   Serial.println(WiFi.localIP());
@@ -279,27 +333,27 @@ void recieveMessage() {
 }
 
 void sendMessage() {
-    String docSerializedJson = serializeJsonStructToMqtt();
-    mqttClient.beginMessage(topic+"-ESP");
-    mqttClient.print(docSerializedJson);
-    mqttClient.endMessage();
+  String docSerializedJson = serializeJsonStructToMqtt();
+  mqttClient.beginMessage(topic + "-ESP");
+  mqttClient.print(docSerializedJson);
+  mqttClient.endMessage();
 }
 void attachPins() {
   pinMode(pinoReleBomba1, OUTPUT);
   pinMode(pinoReleBomba2, OUTPUT);
   pinMode(umidadeSensorBaia1, INPUT);
   pinMode(umidadeSensorBaia2, INPUT);
-  pinMode(nivelSensor, INPUT);
+  // pinMode(nivelSensor, INPUT);
   pinMode(ledBaia1, OUTPUT);
   pinMode(ledBaia2, OUTPUT);
-  servosBay[0].attach(pinDome1);
-  servosBay[1].attach(pinDome2);
+  // servosBay[0].attach(pinDome1);
+  // servosBay[1].attach(pinDome2);
 }
 
 void openBay(Bay* baiaParameter) {
   Serial.print("Abrindo baia de: ");
   Serial.println(baiaParameter->idBay);
-  servosBay[baiaParameter->domeID].write(45);
+  // servosBay[baiaParameter->domeID].write(45);
   baiaParameter->statusDome = true;
   ledsOn(baiaParameter);
 }
@@ -307,7 +361,7 @@ void openBay(Bay* baiaParameter) {
 void closeBay(Bay* baiaParameter) {
   Serial.print("Fechando baia de: ");
   Serial.println(baiaParameter->idBay);
-  servosBay[baiaParameter->domeID].write(0);
+  // servosBay[baiaParameter->domeID].write(0);
   baiaParameter->statusDome = false;
   ledsOff(baiaParameter);
 }
@@ -316,26 +370,27 @@ void ledsOn(Bay* baiaParameter) {
   Serial.print("Ligando luzes de: ");
   Serial.println(baiaParameter->idBay);
   digitalWrite(baiaParameter->pinLight, HIGH);
-  baiaParameter->statusLight = true;
+  // baiaParameter->statusLight = true;
 }
 
 void ledsOff(Bay* baiaParameter) {
   Serial.print("Desligando luzes de: ");
-  Serial.println(baiaParameter->idBay);
   digitalWrite(baiaParameter->pinLight, LOW);
-  baiaParameter->statusLight = false;
+  // baiaParameter->statusLight = false;
 }
 
 void bombOn(Bay* baiaParameter) {
   Serial.print("Ligando bomba de: ");
   Serial.println(baiaParameter->idBay);
   digitalWrite(baiaParameter->pinBomb, HIGH);
+  baiaParameter->statusBomb = true;
 }
 
 void bombOff(Bay* baiaParameter) {
   Serial.print("Desligando bomba de: ");
   Serial.println(baiaParameter->idBay);
   digitalWrite(baiaParameter->pinBomb, LOW);
+  baiaParameter->statusBomb = false;
 }
 
 void updateHumidity(Bay* baiaParameter) {
@@ -348,7 +403,7 @@ void controlHumidity(Bay* baiaParameter) {
   Serial.print("Controlando Umidade de: ");
   Serial.println(baiaParameter->idBay);
   updateHumidity(baiaParameter);
-  if (baiaParameter->umySensorValueDefault >= baiaParameter->umySensorValue && !baiaParameter->statusBomb) {
+  if (baiaParameter->umySensorValueDefault > baiaParameter->umySensorValue && !baiaParameter->statusBomb) {
     bombOn(baiaParameter);
   } else if (baiaParameter->umySensorValueDefault < baiaParameter->umySensorValue && baiaParameter->statusBomb) {
     bombOff(baiaParameter);
@@ -358,21 +413,13 @@ void controlHumidity(Bay* baiaParameter) {
 void controlLight(Bay* baiaParameter) {
   Serial.print("Controlando Luz de: ");
   Serial.println(baiaParameter->idBay);
-  if (baiaParameter->lightTimePickerDefault > baiaParameter->lightTimePicker && !baiaParameter->statusDome) {
-    openBay(baiaParameter);
-    if (!baiaParameter->statusLight) {
+  if (baiaParameter->lightTimePickerDefault > baiaParameter->lightTimePicker) {
       ledsOn(baiaParameter);
-    }
-  } else if (baiaParameter->lightTimePickerDefault <= baiaParameter->lightTimePicker && baiaParameter->statusLight) {
+          Serial.print("Somando 10 segundos na luz de: ");
+    Serial.println(baiaParameter->idBay);
+    baiaParameter->lightTimePicker += 10;
+  } else if (baiaParameter->lightTimePickerDefault <= baiaParameter->lightTimePicker) {
     ledsOff(baiaParameter);
-    if (baiaParameter->statusDome) {
-      closeBay(baiaParameter);
-    }
   }
 
-  if (baiaParameter->statusLight) {
-    Serial.print("Somando 5 segundos na luz de: ");
-    Serial.println(baiaParameter->idBay);
-    baiaParameter->lightTimePicker += 5, 5;
-  }
 }
